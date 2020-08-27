a491c4ea37f8fff2d8858cbfd1821e477c471a69
Seun Oyeniyi <iamseunoyeniyi@gmail.com>
2020-08-27 12:50:56 +0100

    commit

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var path = require('path');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('./public'));
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res) {
+    res.render('front/index');
+});
 
 app.listen(3000, function() {
     console.log("Server running at port 3000, http://localhost:3000 ...");
